7db9f434d4db42e2f9d258763993bb1d5d334333
Cameron Graham <cameron.g882@gmail.com>
2020-08-16 18:47:49 -0400

    forgot to save

---
+++ server.js
@@ -0,0 +1,14 @@
+// require our packages
+const logger = require("morgan");
+const express = require("express");
+const mongoose = require("mongoose");
+
+// use express Middleware ( urlEncoded, json and static ).
+
+// - create PORT and assign to a variable.
+
+// - connect to Mongo Db.
+
+// - import your routes (API and HTML Routes).
+
+// - create code to start the server and listen to requests.
