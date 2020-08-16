114e28b0262a4cb5d08f35a97780a9bcd76d1e22
Cameron Graham <cameron.g882@gmail.com>
2020-08-16 19:12:22 -0400

    created the workout model and ran the seed file to test the database coonectivity, it worked

--- models/workout.js
+++ models/workout.js
@@ -1,13 +1,56 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
 // - include the following properties to the Workout Model:
 //     - day (type -> Date)
 //     - exercises (type -> Array)
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            //     - Inside the "exercises" property Array, you will store each of the exercises for that workout as objects.
+            // - Each object inside the "exercises" property will have the following properties:
+            //     - type (type -> String)
+            //     - name (type -> String)
+            //     - duration (type -> Number)
+            //     - weight (type -> Number)
+            //     - reps (type -> Number)
+            //     - sets (type -> Number)
+            //     - distance (type -> Number)
+            type: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            name: {
+                type: String,
+                required: true,
+                trim: true
+            },
+            duration: {
+                type: Number,
+                required: true,
+                trim: true
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ]
+});
 
-//     - Inside the "exercises" property Array, you will store each of the exercises for that workout as objects.
-// - Each object inside the "exercises" property will have the following properties:
-//     - type (type -> String)
-//     - name (type -> String)
-//     - duration (type -> Number)
-//     - weight (type -> Number)
-//     - reps (type -> Number)
-//     - sets (type -> Number)
-//     - distance (type -> Number)
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
